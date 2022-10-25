dd81ac6e4d07b94f5d15663ee617427c6ab87230
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-10-25 19:03:29 +0530

    Refactored the code

--- controller/food-items/deleteItemController.js
+++
@@ -1,9 +0,0 @@
-const DB = require('../../config/getConnection');
-
-exports.delete = (req, res) => {
-    DB.query('DELETE FROM item WHERE item_id = ?',[req.params.id], (rows, err) => {
-        !err
-          ? res.redirect('/item')
-          : console.log(err);
-      });
-  };
